7649d34ca8f07e503d9399e7c209d5a8b3fa7b7e
deethompson <devonthompson.sl@gmail.com>
2020-02-12 07:17:34 -0800

    first commit this project is not functional currently debugging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UvComponent } from './uv/uv.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { ForecastComponent } from './forecast/forecast.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UvComponent,
+    CurrentWeatherComponent,
+    ForecastComponent,
+    HomePageComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
